e972568e74c2cc43ca54bf6a1bbeff0e08d14896
Sahar Ali <saharnazimali@gmail.com>
2023-10-07 11:47:03 -0400

    Adds score

--- snake.py
+++ snake.py
@@ -115,6 +115,7 @@ class MAIN:
         self.draw_grass()
         self.fruit.draw_fruit()
         self.snake.draw_snake()
+        self.draw_score()
     
     def check_collision(self):
         if self.fruit.pos == self.snake.body[0]:
@@ -151,18 +152,31 @@ class MAIN:
                     if col % 2 != 0:
                         grass_rect = pygame.Rect(col * cell_size,row * cell_size,cell_size, cell_size)
                         pygame.draw.rect(screen, grass_color, grass_rect)
+    
+    def draw_score(self):
+        score_text = str(len(self.snake.body) - 3)
+        score_surface = game_font.render(score_text, True, (56, 74, 12))
+        score_x = int(cell_size * cell_number - 30)
+        score_y = int(cell_size * cell_number - 20)
+        score_rect = score_surface.get_rect(center = (score_x, score_y))
+        apple_rect = apple.get_rect(midright = (score_rect.left, score_rect.centery))
+        bg_rect = pygame.Rect(apple_rect.left, apple_rect.top,apple_rect.width + score_rect.width + 6,apple_rect.height + 3)
+        pygame.draw.rect(screen, (167, 209, 61), bg_rect)
+        screen.blit(score_surface, score_rect)
+        screen.blit(apple, apple_rect)
+        pygame.draw.rect(screen, (56, 74, 12), bg_rect, 2)
 
 # starts all the modules (sounds, graphics,etc)
 pygame.init()
 cell_number = 40
-cell_size = 40
+cell_size = 20
 # need to create the display surface (main game window)
-# creates window 400 pixels wide by 500 pixels high
 screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
 # clock object that helps influence time in game
 clock = pygame.time.Clock()
 apple = pygame.image.load('Graphics/apple.png').convert_alpha()
 
+game_font = pygame.font.Font('Font/PoetsenOne-Regular.ttf', 25)
 SCREEN_UPDATE = pygame.USEREVENT
 # this event is trigger every 150 ms
 pygame.time.set_timer(SCREEN_UPDATE, 150)
